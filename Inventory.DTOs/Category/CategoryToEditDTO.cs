5f2076bf2e89b67aa2dc6e269c539054e2be9d5b
Wifix-dev <152722579+Wifix-dev@users.noreply.github.com>
2024-02-12 15:38:40 -0600

    DTOs Faltantes

--- Inventory.DTOs/Category/CategoryToEditDTO.cs
+++ Inventory.DTOs/Category/CategoryToEditDTO.cs
@@ -3,7 +3,7 @@ namespace Inventory.DTOs.Category
     public class CategoryToEditDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
-        public string Description { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
